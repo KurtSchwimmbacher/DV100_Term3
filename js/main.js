18bba72b934701d4b2d835ab7158e12a4f063941
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-07-17 17:33:08 +0200

    Class week 2
    add bootstrap and use jquery to animate carousel component

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//When the document loads
+$(document).ready(function(){
+
+    console.log("Hello World")
+
+    //When the document loads animate hero image upwards
+    $(".hero-image").animate({top:'-=100'});
+
+});
