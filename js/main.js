a1d49bf70b7450f6051d516d0d1da9176374e72a
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-08-07 17:47:48 +0200

    follow along in class
    filter and sort like along in class

--- js/main.js
+++ js/main.js
@@ -8,44 +8,59 @@ const plantArr = [
         name : "Fikus Tree",
         price : 350,
         description : "Graceful and lush, this charming indoor plant boasts glossy, emerald-green leaves that effortlessly brighten any space.",
-        image : "plant1.png"
+        image : "plant1.png" ,
+        lightAmount:"low",
+        addedDate: "2023-03-25"
     },
     {
         name : "White Sprite Succulent",
         price : 200,
         description : "Delicate and captivating, this rare succulent showcases a mesmerizing silver-white hue that gracefully adorns its petite, fleshy leaves.",
-        image : "plant2.png"
+        image : "plant2.png",
+        lightAmount:"bright",
+        addedDate: "2023-05-01"
     },
     {
         name : "Snake Plant",
         price : 400,
         description : "Boasting tall, sleek, and sword-like leaves, this botanical marvel adds a touch of modern flair to any setting.",
-        image : "plant3.png"
+        image : "plant3.png",
+        lightAmount:"low",
+        addedDate: "2023-07-14"
     },
     {
         name : "Parlour Palm",
         price : 350,
         description : "With its lush, feather-like fronds and compact size, this indoor beauty makes a striking addition to any interior space.",
-        image : "plant4.png"
+        image : "plant4.png",
+        lightAmount:"low",
+        addedDate: "2023-07-04"
     },
     {
         name : "Japanese Maple",
         price : 1200,
         description : "Known for its stunning foliage that transforms with the seasons, this ornamental tree captivates with its delicate, lacy leaves in vibrant shades of red, orange, or gold.",
-        image : "plant5.png"
+        image : "plant5.png",
+        lightAmount:"bright",
+        addedDate: "2023-04-29"
     }
 ];
 
-
+    let appliedFilter = "";
+    let appliedSort = "date added";
 
 // functions
 // =============================================================================================================
 
 // load all plants on website
-loadPlants = () =>{
+function loadPlants (plantsToShow) {
     
-    for(let i =0; i < plantArr.length;i++){
-        const plant = plantArr[i];
+    //Clear all elements in plants container before loading new ones
+    $("#plantsContainer").empty();
+
+
+    for(let i =0; i < plantsToShow.length;i++){
+        const plant = plantsToShow[i];
 
         console.log(plant);
 
@@ -56,7 +71,7 @@ loadPlants = () =>{
         $("#plantsContainer").append($("#plantCardTemplate").html());
 
         // Create a variable that contains the most recently added card
-        let currentChild = $("#plantsContainer").children().eq(i+1);
+        let currentChild = $("#plantsContainer").children().eq(i);
 
         // Set the content for the current plant card from the plant array
         $(currentChild).find("#nameText").text(plant.name);
@@ -75,7 +90,7 @@ loadPlants = () =>{
         $("#wishlist-plant-container").append($("#wishlist-table-template").html());
 
         // Create a bew variable for the most recently added row
-        let newChild = $("#wishlist-plant-container").children().eq(i+1);
+        let newChild = $("#wishlist-plant-container").children().eq(i);
 
         // Set the content for the current plant into the table from the plant array
         // $(newChild).find("#tableRow").text(i+1);
@@ -124,7 +139,7 @@ $(document).ready( ()=>{
 // ==========================================================================================================
 
         // load the plant function
-        loadPlants();
+        filterSortPlants(plantArr);
 
         $("#plantsContainer").on('click','.card', function (){
             //Toggle the price and description text
@@ -137,6 +152,61 @@ $(document).ready( ()=>{
             $(this).find($(".card-img-top")).toggleClass("small");
         });
 
+
+        
+    // ==========================================================================
+    // When Sort / filter is clicked it applies
+    // ==========================================================================
+
+        $("input[name= 'filterRadio']").click(function(){
+            appliedFilter = $(this).attr('value');
+
+            console.log(appliedFilter);
+            filterSortPlants()
+        });
+
+
+        $("input[name = 'sortRadio']").click( function(){
+            appliedSort = $(this).attr('value');
+
+            console.log(appliedSort);
+            filterSortPlants()
+        });
+
+
+    
+
+        function filterSortPlants(){
+            let filSorPlantArr = [];
+
+            // Filter Plants
+
+            if(appliedFilter){
+                filSorPlantArr = plantArr.filter(plant =>plant.lightAmount == appliedFilter);
+            }   else{
+                filSorPlantArr = plantArr;
+            }
+
+            // sort
+            if(appliedSort == "low to high"){
+                // sort the plants from lowest to highest price
+                filSorPlantArr = filSorPlantArr.sort((a,b) =>{
+                    return a.price - b.price;
+                });
+            }
+            else if(appliedSort == "date added"){
+                //sort by added date, newest to oldest
+                filSorPlantArr = filSorPlantArr.sort((a,b) =>{
+                    let da = new Date(a.addedDate);
+                    let db = new Date(b.addedDate);
+                    
+                    return db-da;
+                });
+            }
+
+
+            loadPlants(filSorPlantArr);
+        }
 
 //Wishlist Page 
 // ==========================================================================================================
@@ -162,7 +232,5 @@ $(document).ready( ()=>{
     });
 
 });
-
-
 
 
